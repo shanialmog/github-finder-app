4b950be22d9e6fad6cab5641458234502c23220c
Shani Almog <killerdrum@gmail.com>
2021-02-26 22:41:15 +0200

    working on the user item

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Component } from 'react'
 import './App.css'
 
 import Navbar from './components/Navbar'
+import UserItem from './components/UserItem'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <UserItem />
       </div>
     )
   }
--- src/components/UserItem/index.js
+++ src/components/UserItem/index.js
@@ -1,9 +1,20 @@
 import { Component } from 'react'
 
 class UserItem extends Component {
+    constructor() {
+        super()
+        this.state = {
+            id: 'id',
+            login: 'mojombo',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+            html_url: 'https://github.com/mojombo'
+        }
+    }
     render() {
-        return(
-            
+        return (
+            <div>
+                <img src={this.state.avatar_url} alt="" className="round-img"/>
+            </div>
         )
     }
 }
