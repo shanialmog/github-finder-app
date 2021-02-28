5321df10e8357905d4570c49fced49f424537aaf
Shani Almog <killerdrum@gmail.com>
2021-02-28 16:34:55 +0200

    add clear button for user search

--- src/App.js
+++ src/App.js
@@ -12,19 +12,34 @@ class App extends Component {
     isLoading: false
   }
 
-  async componentDidMount() {
+  searchUsers = async (text) => {
     this.setState({ isLoading: true })
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}$client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-    this.setState({ users: res.data, isLoading: false })
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}$client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    this.setState({ users: res.data.items, isLoading: false })
   }
 
+  clearUsers = () => {
+    this.setState({ users: [], isLoading: false })
+  }
+
+  // isDisabled = () => {
+  //   console.log("wheee")
+  //   return (this.state.users.length > 0 ? false : true)
+  // }
+
   render() {
+    const isDisabled = this.state.users.length > 0 ? false : true
     return (
       <div>
         <Navbar />
         <div className="container">
-        <SearchBar />
-          <Users users={this.state.users} isLoading={this.state.isLoading} />
+          <SearchBar
+          searchUsers={this.searchUsers}
+          clearUsers={this.clearUsers}
+          isDisabled={isDisabled} />
+          <Users
+          users={this.state.users}
+          isLoading={this.state.isLoading} />
         </div>
       </div >
     )
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,13 +1,50 @@
 import { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class SearchBar extends Component {
+    state = {
+        text: ''
+    }
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired
+    }
+
+    onChange = (e) => {
+        const { name, value } = e.target
+        this.setState({ [name]: value })
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.props.searchUsers(this.state.text)
+        this.setState({ text: '' })
+    }
+
     render() {
         return (
-            <div>
-                <form  className="search">
-                    <input type="text" name="text" placeholder="Search users" />
-                    <input type="submit" value="Search" className="btn" />
+            <div className="search-cont">
+                <form
+                    className="search"
+                    onSubmit={this.onSubmit}>
+                    <input
+                        type="text"
+                        name="text"
+                        value={this.state.text}
+                        onChange={this.onChange}
+                        placeholder="Search users" />
+                    <input
+                        type="submit"
+                        value="Search"
+                        className="btn search-btn" />
                 </form>
+                <button
+                    className="btn clear-btn"
+                    onClick={this.props.clearUsers}
+                    disabled={this.props.isDisabled} >
+                    Clear
+                </button>
             </div>
         )
     }
